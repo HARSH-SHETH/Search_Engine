ce60f719fcec0411a9eff3037e3d87cef852853a
HARSH-SHETH <ycdtharsh@gmail.com>
2020-07-17 20:22:46 +0530

    search function added

--- project.cpp
+++ project.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <ctype.h>
+#include <sstream>
+
+std::string lowercase(std::string &s){
+  for(int i = 0; i < s.length(); i++){
+    s[i] = std::tolower(s[i]);
+  }
+  return s;
+}
+
+bool search_substring(std::string sub_string, std::string main_string){
+  std::stringstream ss(main_string);
+  std::string word;
+  while(ss >> word){
+    lowercase(word);
+    if(word == sub_string)
+      return true;
+  }
+  return false;
+}
 
 struct url_node{ 
   std::string url;
@@ -17,7 +37,10 @@ class SEARCH_ENGINE{
     SEARCH_ENGINE(): head{NULL}, tail{NULL}, list_of_urls{""}, url_matched{""}, keywords{""} {}
     void get_search_query(); // GET QUERY ARRAY CONTAINING KEYWORDS
     void fetch_urls();  // READ ALL URLS FROM URLS.TXT AND MAKE A VECTOR OF STRINGS
-    bool linear_search(std::string);  // RETURN TRUE IF THE URL CONTAIN THE KEYWORD
+    
+    // Return the vector containing urls or return not found as a first element of the array
+    std::vector<std::string> search_engine(std::vector<std::string>);  
+
     void insert_url(std::string, int);   // MAKE A NEW NODE AND INSERT URL ACC. TO PRIORITY
     void write_url();   // WRITE SELECTED URL TO THE FILE: LINK.TXT
     int display_urls(); // DISPLAY THE LIST OF URLS MATCHING THE KEYWORD AND RETURN THE OPTION SELECTED
@@ -41,16 +64,32 @@ void SEARCH_ENGINE::get_search_query(){
   std::string keyword = "";
   for(int i = 0; i <= s.size(); i++){
     if(s[i] != ' ' && s[i] != '\0'){
-      keyword += s[i];
+      keyword += tolower(s[i]);
     }else{
       keywords.push_back(keyword);
       keyword = "";
     }
   }
 }
+std::vector<std::string> SEARCH_ENGINE::search_engine(std::vector<std::string> query){
+  std::ifstream fin;
+  std::string word, line;
+  for(auto x:keywords){
+    fin.open("Generic_Problem.txt", std::ios::in);
+    while(!fin.eof()){
+      getline(fin, line);
+      if(search_substring(x, line)){
+        std::cout << line << "\n";
+      }
+    }
+    fin.close();
+  }
+  return query;
+}
 int main(){
   SEARCH_ENGINE S;
   S.get_search_query();
-  S.fetch_urls();
+  S.search_engine({"harsh"});
+/*  S.fetch_urls();*/
   return 0;
 }
